ff21968f0327c3b5bcc70b07d48e29136babec2f
NoobiesDoobies <carlioseryan20@gmail.com>
2025-02-19 22:20:20 +0700

    add widget ui

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,46 @@
 #include "Boid.hpp"
 #include "Flock.hpp"
 
-#define WINDOW_HEIGHT 480 
-#define WINDOW_WIDTH  720
+#define WINDOW_HEIGHT 480
+#define WINDOW_WIDTH 720
 
-int main() {
+#define MENU_WIDTH 240
+#define MENU_HEIGHT WINDOW_HEIGHT
+sf::Color hex2color(const std::string &hexcolor)
+{
+    sf::Color color = sf::Color::White;
+    if (hexcolor.size() == 7) // #ffffff
+    {
+        color.r = strtoul(hexcolor.substr(1, 2).c_str(), NULL, 16);
+        color.g = strtoul(hexcolor.substr(3, 2).c_str(), NULL, 16);
+        color.b = strtoul(hexcolor.substr(5, 2).c_str(), NULL, 16);
+    }
+    else if (hexcolor.size() == 4) // #fff
+    {
+        color.r = strtoul(hexcolor.substr(1, 1).c_str(), NULL, 16) * 17;
+        color.g = strtoul(hexcolor.substr(2, 1).c_str(), NULL, 16) * 17;
+        color.b = strtoul(hexcolor.substr(3, 1).c_str(), NULL, 16) * 17;
+    }
+    return color;
+}
+
+void clearScreen(sf::RenderWindow *window, sf::Color sim_bg_color, sf::Color slider_bg_color)
+{
+    sf::RectangleShape sim({WINDOW_WIDTH, WINDOW_HEIGHT});
+    sf::RectangleShape slider({MENU_WIDTH, MENU_HEIGHT});
+
+    sim.setPosition({0, 0});
+    sim.setFillColor(sim_bg_color);
+
+    slider.setPosition({WINDOW_WIDTH, 0});
+    slider.setFillColor(slider_bg_color);
+
+    window->draw(sim);
+    window->draw(slider);
+}
+
+int main()
+{
     // Create Config
     FlockConfig config;
     config.numBoids = 50;
@@ -24,9 +60,9 @@ int main() {
     config.alignmentWeight = 1.0;
     config.cohesionWeight = 1.0;
     config.boidRadius = 7.5;
-    config.separationRadius = WINDOW_WIDTH/10;
-    config.alignmentRadius = WINDOW_WIDTH/5;
-    config.cohesionRadius = WINDOW_WIDTH/5;
+    config.separationRadius = WINDOW_WIDTH / 10;
+    config.alignmentRadius = WINDOW_WIDTH / 5;
+    config.cohesionRadius = WINDOW_WIDTH / 5;
 
     // Initialize Flock
     std::cout << "Creating Flock..." << std::endl;
@@ -34,39 +70,59 @@ int main() {
     std::cout << "Flock Created!" << std::endl;
 
     // Initialize SFML Window
-    sf::RenderWindow App(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Boid Flocking Simulation");
+    sf::RenderWindow App(sf::VideoMode(WINDOW_WIDTH + MENU_WIDTH, WINDOW_HEIGHT), "Boid Flocking Simulation");
 
-    if (!App.isOpen()) {
+    if (!App.isOpen())
+    {
         std::cerr << "SFML Window failed to open!" << std::endl;
         return EXIT_FAILURE;
     }
 
+    // Create widgets
+    gui::Menu menu(App);
+    gui::Theme::loadFont("widget_styles/tahoma.ttf");
+    gui::Theme::loadTexture("widget_styles/texture-default.png");
+    gui::Theme::windowBgColor = hex2color("#d4d0c8");
+    gui::HBoxLayout *hbox = menu.addHBoxLayout();
+    gui::FormLayout *form = hbox->addFormLayout();
+
+    menu.setPosition(WINDOW_WIDTH, 0);
+
+    // Create slider
+    gui::Slider *test_slider = new gui::Slider();
+    test_slider->setStep(1);
+    test_slider->setCallback([&]()
+                             { std::cout << "Value is " << test_slider->getValue() << std::endl; });
+    form->addRow("Test Slider", test_slider);
 
     // Main Loop
     sf::Clock clock;
-    while (App.isOpen()) {
+    while (App.isOpen())
+    {
         // Handle Events
         sf::Event event;
-        while (App.pollEvent(event)) {
+        while (App.pollEvent(event))
+        {
+            menu.onEvent(event);
             if (event.type == sf::Event::Closed)
                 App.close();
-            
         }
 
         // Draw slider
 
-
         // Update Simulation
         float dt = clock.restart().asSeconds();
         flock.applyFlockingRules();
-        flock.update();  // Ensure update function accepts delta time
+        flock.update(); // Ensure update function accepts delta time
 
         // Render
-        App.clear();
+        // App.clear(hex2color("#d4d0c8"));
+        clearScreen(&App, sf::Color::Black, hex2color("#d4d0c8"));
+        App.draw(menu);
         flock.render(App);
         App.display();
 
-        sf::sleep(sf::milliseconds(16));  // Maintain ~60 FPS
+        sf::sleep(sf::milliseconds(16)); // Maintain ~60 FPS
     }
 
     return EXIT_SUCCESS;
